b424888d466a6ca4e733e092add5bfe830f5a8f1
Rezwan <t.rewan17@gmail.com>
2023-06-10 16:45:49 +0600

    Make admin api

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p9pdn5v.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -71,6 +71,19 @@ async function run() {
             return res.send({message: 'User already exist'})
         }
         const result = await userCollection.insertOne(user)
+        res.send(result)
+    })
+
+    // make admin
+    app.patch('/users/admin/:id', async(req,res)=>{
+        const id = req.params.id;
+        const filter = {_id: new ObjectId(id)}
+        const updatedDoc ={
+            $set:{
+                role: 'admin'
+            }
+        }
+        const result = await userCollection.updateOne(filter, updatedDoc)
         res.send(result)
     })
 
